c8cc1b0c5a296c9fa899a5563dc5851c7dcbd2c0
hr3003 <hr3003@att.com>
2024-04-20 02:48:47 +0530

    added Acetinity Ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import WorkLinks from '@/components/WorkLinks'
+import { SparklesCore } from '@/components/ui/sparkles'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className='bg-black'>
+    <main className=''>
+      
       <Navbar/>
       <Hero/>
       <ContactLinks/>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { SparklesCore } from "./ui/sparkles";
 
 const Hero = () => {
   return (
     <div className="HeroSection flex justify-center pt-20 pb-10">
+        <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={15}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+        </div>
       <p className="text-4xl  leading-[39px] text-center bg-clip-text max-w-[337px] bg-gradient-to-b from-white to-gray-400 text-transparent">
         a third culture creative bringing ideas into reality, building digital
         products and documenting it all.
