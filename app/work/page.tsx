2ba49281064be7888f5f6832e66b4b841e9f566b
hr3003 <hr3003@att.com>
2024-04-20 18:13:37 +0530

    Padding fix

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -11,7 +11,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=' w-full h-full flex justify-center items-center md:pt-[80px]'>
+    <div className=' w-full h-full flex justify-center items-center md:pt-[20px]'>
       <div className='max-w-7xl mx-auto rounded-md] '>
         
         <Cssgrid/>
